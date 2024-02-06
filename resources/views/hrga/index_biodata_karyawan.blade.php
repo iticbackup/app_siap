86da4df5684844be046823289878b58e2f2ca66d
iticbackup <ict.indonesiantobacco.backup@gmail.com>
2024-02-06 15:40:45 +0700

    Siap

--- resources/views/hrga/index_biodata_karyawan.blade.php
+++ resources/views/hrga/index_biodata_karyawan.blade.php
@@ -159,7 +159,7 @@
                 // { className: 'text-right', targets: [7, 10, 11, 14, 16] },
                 {
                     className: 'text-center',
-                    targets: [0,1,3,4,5,6]
+                    targets: [0,1,3,4,5,6,7]
                 },
             ],
         });
