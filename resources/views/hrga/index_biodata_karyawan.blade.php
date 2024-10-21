b9d03c75562ae239728fd67488317287feb2f7b1
Rio Anugrah Adam Saputra <ict.indonesiantobacco.backup@gmail.com>
2024-10-21 14:29:05 +0700

    Siap

--- resources/views/hrga/index_biodata_karyawan.blade.php
+++ resources/views/hrga/index_biodata_karyawan.blade.php
@@ -156,10 +156,10 @@
             ],
             columnDefs: [
                 // { className: 'text-right', targets: [7, 10, 11, 14, 16] },
-                // {
-                //     className: 'text-center',
-                //     targets: [0,1,3,4,5,6,7]
-                // },
+                {
+                    className: 'text-center',
+                    targets: [0,1,3,4,5,6,7]
+                },
             ],
         });
 
