753c4feeb885df25fe53004cec359c6d894d7d4a
Rio Anugrah Adam Saputra <ict.indonesiantobacco.backup@gmail.com>
2025-08-11 10:05:31 +0700

    absensi

--- resources/views/layouts/auth/absensi/master.blade.php
+++ resources/views/layouts/auth/absensi/master.blade.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="{{ asset('public/absensi/new/') }}/css/main.css" />
     <script src="{{ asset('public/absensi/new/') }}/js/base/loader.js"></script>
 
-    <title>@yield('title')</title>
+    <title>Login Absensi</title>
 </head>
 
 <body class="h-100">
@@ -31,20 +31,7 @@
                 <div class="offset-0 col-12 d-none d-lg-flex offset-md-1 col-lg h-lg-100">
                     <div class="min-h-100 d-flex align-items-center">
                         <div class="w-100 w-lg-75 w-xxl-50">
-                            <div>
-                                <div class="mb-5">
-                                    <h1 class="display-3 text-white">Multiple Niches</h1>
-                                    <h1 class="display-3 text-white">Ready for Your Project</h1>
-                                </div>
-                                <p class="h6 text-white lh-1-5 mb-5">
-                                    Dynamically target high-payoff intellectual capital for customized technologies.
-                                    Objectively integrate emerging core competencies before
-                                    process-centric communities...
-                                </p>
-                                <div class="mb-5">
-                                    <a class="btn btn-lg btn-outline-white" href="index.html">Learn More</a>
-                                </div>
-                            </div>
+                            
                         </div>
                     </div>
                 </div>
@@ -55,11 +42,6 @@
                     <div
                         class="sw-lg-70 min-h-100 bg-foreground d-flex justify-content-center align-items-center shadow-deep py-5 full-page-content-right-border">
                         <div class="sw-lg-50 px-5">
-                            <div class="sh-11">
-                                <a href="index.html">
-                                    <div class="logo-default"></div>
-                                </a>
-                            </div>
                             <div class="mb-5">
                                 <h2 class="cta-1 mb-0 text-primary">Selamat Datang di,</h2>
                                 <h2 class="cta-1 text-primary">Absensi ITIC</h2>
